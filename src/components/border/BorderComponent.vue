2f750ffbc869f138b1a30fbf296c14cdedb87dae
Cromi <casel1990@gmail.com>
2018-08-15 15:46:18 -0500

    stiles and fixed

--- src/components/border/BorderComponent.vue
+++ src/components/border/BorderComponent.vue
@@ -12,9 +12,9 @@
 					<p><strong>Nota:</strong> Para que el borde se visualize se debe especificar la propiedad <strong>border-style</strong> (en este caso es solid), por default el color de borde es negro.</p>
 				</div>
 				<div class="code margin-vertical">
-					<div>div { <p>border: {{border}}px solid gray;</p>}</div>
+					<div>div { <p>border: {{border}}px solid #8e44ad;</p>}</div>
 					<div class="small-jump">equal to</div>
-					<div>div {<p>border-style: solid;</p><p> border-width: {{border}}px;</p><p>border-color: gray;</p>}</div>
+					<div>div {<p>border-style: solid;</p><p> border-width: {{border}}px;</p><p>border-color: #8e44ad;</p>}</div>
 				</div>
 				<div class="options margin-vertical">
 					<p><label>border:</label> <input type="range" v-model="border" min="0" max="20"></p>
@@ -37,7 +37,7 @@
 					<div>div { border-style: {{tBorder}}; }</div>
 				</div>
 				<div class="options margin-vertical">
-					<p><label>tipo de borde:</label>
+					<p><label>estilo:</label>
 						<select v-model="tBorder">
 							<option value="solid">solid</option>
 							<option value="dotted">dotted</option>
@@ -63,7 +63,7 @@
 			<div class="description-container">
 				<h3 class="center-text">individual sides</h3>
 				<div class="note definition">
-					<p>Cada lado del elemento HTML puede tener su propio estilo como lo muestra el ejemplo.</p>
+					<p>Cada lado del elemento HTML puede tener su propio estilo de borde como lo muestra el ejemplo.</p>
 				</div>
 				<div class="code margin-vertical">
 					<div>div { border-style: solid dotted double dashed; }</div>
@@ -80,13 +80,13 @@
 			<div class="description-container">
 				<h3 class="center-text">rounded borders</h3>
 				<div class="note important">
-					<p><strong>Nota:</strong> Cada lado del borde se puede redondear individualmente. La propiedad <strong><em>border-radius</em></strong>, a diferencia de <em>border-width</em> no necesita tener definida la propiedad <em>border-style</em>, es decir, no es necesario definir un borde</p>
+					<p><strong>Nota:</strong> Cada lado del borde se puede redondear individualmente. La propiedad <strong><em>border-radius</em></strong>, a diferencia de <em>border-width</em> no necesita tener definida la propiedad <em>border-style</em>, es decir, no es necesario definir un estilo de borde</p>
 				</div>
 				<div class="code margin-vertical">
 					<div>div { border-radius: {{radius}}%; }</div>
 				</div>
 				<div class="options margin-vertical">
-					<p><label>border radius:</label> <input type="range" v-model="radius" min="0" max="50"></p>
+					<p><label>radius:</label> <input type="range" v-model="radius" min="0" max="50"></p>
 				</div>
 			</div>
 			<div class="example-container flex-center">
